d9a07fd25d63fcf13f29d2ff11ed642dc4b9119a
ZENHOM <you@example.com>
2023-03-14 21:20:05 +0200

    remove rastrict mode

--- src/main.jsx
+++ src/main.jsx
@@ -44,9 +44,7 @@ const router = createBrowserRouter([
   },
 ]);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
